4026f43f687afcec447ba5c7ff066e33c0014d86
ControlPro2 <40320474+ControlPro2@users.noreply.github.com>
2019-12-28 23:26:24 +0000

    Update Metzo's Sine Graph Slider Velocity Program.py

--- Metzo's Sine Graph Slider Velocity Program.py
+++ Metzo's Sine Graph Slider Velocity Program.py
@@ -1,16 +1,31 @@
 #ive never made a program before so this is probs riddled with mistakes
-#THIS IS ONLY FOR MAKING A SLOW TO FAST SLOWDOWN OR SLOW TO FAST UPRISE
+#make sure to not be stupid when inputting values - make sure it makes sense and isnt impossible
 
-import math
-StartSV = float(input("what's the starting sv: "))
-EndSV = float(input("what's the ending sv: "))
-NoteAmount = float(input("how many notes are there: "))
-NoteAmount = NoteAmount - 1
-SVRange = StartSV - EndSV
-SV = 1000
-x = 1
-print(x, StartSV)
-while x != NoteAmount + 1:
-    SV = SVRange * math.sin(0.5 * math.pi * (x / NoteAmount) + math.pi / 2) + EndSV
-    x = x + 1
-    print(x, SV)
+input = input("Press 1 for Slow to Fast Slowdown/Uprise. Press 2 for Fast to Slow Slowdown/Uprise.")
+if input == "1":
+    StartSV = float(input("what's the starting sv: "))
+    EndSV = float(input("what's the ending sv: "))
+    NoteAmount = float(input("how many notes are there: "))
+    NoteAmount = NoteAmount - 1
+    SVRange = StartSV - EndSV
+    SV = 1000
+    x = 1
+    print(x, StartSV)
+    while x != NoteAmount + 1:
+        SV = SVRange * math.sin(0.5 * math.pi * (x / NoteAmount) + math.pi / 2) + EndSV
+        x = x + 1
+        print(x, SV)
+
+if input =="2":
+    StartSV = float(input("what's the starting sv: "))
+    EndSV = float(input("what's the ending sv: "))
+    NoteAmount = float(input("how many notes are there: "))
+    NoteAmount = NoteAmount - 1
+    SVRange = EndSV - StartSV
+    SV = 1000
+    x = 1
+    print(x, StartSV)
+    while x != NoteAmount + 1:
+        SV = SVRange * math.sin(0.5 * math.pi * (x / NoteAmount) + math.pi / 2) + EndSV
+        x = x + 1
+        print(x, SV)
